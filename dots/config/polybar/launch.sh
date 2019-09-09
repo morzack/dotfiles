6266a041dff683d94e2934c5631e0f6545643c8c
morzack <john_kesler@caryacademy.org>
2019-09-08 21:36:08 -0400

    Updated

--- dots/config/polybar/launch.sh
+++ dots/config/polybar/launch.sh
@@ -3,4 +3,10 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar example
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
